e028caf1fdb95cb49456775700739ea0217ba319
Allan He-Chen <allanhechen@gmail.com>
2026-03-04 20:18:36 -0500

    Add backend types

--- backend/src/routes/updateItem.ts
+++ backend/src/routes/updateItem.ts
@@ -2,10 +2,12 @@ import db from '../persistence/index.js';
 import type { Request, Response } from 'express';
 
 export default async (req: Request, res: Response) => {
-    await db.updateItem(req.params.id, {
+    const id = req.params.id as string;
+    await db.updateItem(id, {
+        id: id,
         name: req.body.name,
         completed: req.body.completed,
     });
-    const item = await db.getItem(req.params.id);
+    const item = await db.getItem(id);
     res.send(item);
 };
