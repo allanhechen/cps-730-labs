5d1ae3b977e196e4f60dbd93b3e8a80d0d2b935f
Allan He-Chen <allanhechen@gmail.com>
2026-03-05 09:50:19 -0500

    Migrate to promise based sqlite

--- backend/src/persistence/sqlite.ts
+++ backend/src/persistence/sqlite.ts
@@ -4,113 +4,70 @@ dotenv.config();
 
 import sqlite3 from 'sqlite3';
 sqlite3.verbose();
+import { open, type Database } from 'sqlite';
 import fs from 'fs';
 const location = process.env.SQLITE_DB_LOCATION || '/etc/todos/todo.db';
 import path from 'path';
 import type { Todo } from '@todo-app/shared';
 
-let db: sqlite3.Database;
+let db: Database;
 
-function init(): Promise<void> {
+async function init(): Promise<any> {
     const dirName = path.dirname(location);
     if (!fs.existsSync(dirName)) {
         fs.mkdirSync(dirName, { recursive: true });
     }
 
-    return new Promise((acc, rej) => {
-        db = new sqlite3.Database(location, (err) => {
-            if (err) return rej(err);
-
-            if (process.env.NODE_ENV !== 'test')
-                console.log(`Using sqlite database at ${location}`);
-
-            db.run(
-                'CREATE TABLE IF NOT EXISTS todo_items (id varchar(36), name varchar(255), completed boolean)',
-                (err: any) => {
-                    if (err) return rej(err);
-                    acc();
-                },
-            );
-        });
+    db = await open({
+        filename: '/tmp/database.db',
+        driver: sqlite3.Database,
     });
+
+    await db.run(
+        'CREATE TABLE IF NOT EXISTS todo_items (id varchar(36), name varchar(255), completed boolean, priority integer, utcDueDate text)',
+    );
 }
 
 async function teardown(): Promise<void> {
-    return new Promise((acc, rej) => {
-        db.close((err) => {
-            if (err) rej(err);
-            else acc();
-        });
-    });
+    await db.close();
 }
 
 async function getItems() {
-    return new Promise((acc, rej) => {
-        db.all('SELECT * FROM todo_items', (err, rows: Todo[]) => {
-            if (err) return rej(err);
-            acc(
-                rows.map((item) =>
-                    Object.assign({}, item, {
-                        completed: (item.completed as any) === 1,
-                    }),
-                ),
-            );
-        });
-    });
+    const rows = await db.all('SELECT * FROM todo_items');
+    return rows.map((item) =>
+        Object.assign({}, item, {
+            completed: (item.completed as any) === 1,
+        }),
+    );
 }
 
 async function getItem(id: Todo['id']) {
-    return new Promise((acc, rej) => {
-        db.all(
-            'SELECT * FROM todo_items WHERE id=?',
-            [id],
-            (err, rows: Todo[]) => {
-                if (err) return rej(err);
-                acc(
-                    rows.map((item) =>
-                        Object.assign({}, item, {
-                            completed: (item.completed as any) === 1,
-                        }),
-                    )[0],
-                );
-            },
-        );
-    });
+    const rows = await db.all('SELECT * FROM todo_items WHERE id=?', [id]);
+
+    return rows.map((item) =>
+        Object.assign({}, item, {
+            completed: (item.completed as any) === 1,
+        }),
+    )[0];
 }
 
 async function storeItem(item: Todo): Promise<void> {
-    return new Promise((acc, rej) => {
-        db.run(
-            'INSERT INTO todo_items (id, name, completed) VALUES (?, ?, ?)',
-            [item.id, item.name, item.completed ? 1 : 0],
-            (err) => {
-                if (err) return rej(err);
-                acc();
-            },
-        );
-    });
+    await db.run(
+        'INSERT INTO todo_items (id, name, completed) VALUES (?, ?, ?)',
+        [item.id, item.name, item.completed ? 1 : 0],
+    );
 }
 
 async function updateItem(id: Todo['id'], item: Todo): Promise<void> {
-    return new Promise((acc, rej) => {
-        db.run(
-            'UPDATE todo_items SET name=?, completed=? WHERE id = ?',
-            [item.name, item.completed ? 1 : 0, id],
-            (err) => {
-                if (err) return rej(err);
-                acc();
-            },
-        );
-    });
+    await db.run('UPDATE todo_items SET name=?, completed=? WHERE id = ?', [
+        item.name,
+        item.completed ? 1 : 0,
+        id,
+    ]);
 }
 
 async function removeItem(id: Todo['id']): Promise<void> {
-    return new Promise((acc, rej) => {
-        db.run('DELETE FROM todo_items WHERE id = ?', [id], (err) => {
-            if (err) return rej(err);
-            acc();
-        });
-    });
+    await db.run('DELETE FROM todo_items WHERE id = ?', [id]);
 }
 
 export default {
